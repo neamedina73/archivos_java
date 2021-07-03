e4809fc7b124cde5299b0db642e4970c7535a076
neamedina73 <77454466+neamedina73@users.noreply.github.com>
2021-07-03 16:39:09 -0500

    Add files via upload

--- Ejercicios basicos de Java/Lee_dos_numeros_ciclo.java
+++ Ejercicios basicos de Java/Lee_dos_numeros_ciclo.java
@@ -3,15 +3,15 @@ public class Lee_dos_numeros_ciclo{
     public static void main(String[] args) {
         
         Scanner leernumero = new Scanner(System.in);
-        int numero1 = 0;
-        int numero2 = 0;
+        int num1 = 0;
+        int num2 = 0;
         do{
             System.out.println("Ingrese el número 1: ");
-            numero1 = leernumero.nextInt();
+            num1 = leernumero.nextInt();
             System.out.println("Ingrese el número 2: ");
-            numero2 = leernumero.nextInt();
-        }while(numero1!=numero2);
-        System.out.println("Has terminado, los números son iguales");
+            num2 = leernumero.nextInt();
+        }while(num1!=num2);
+        System.out.println("Has terminado, los números son iguales, el "+num1+ " es igual a " +num2);
         leernumero.close();
     }
 }
