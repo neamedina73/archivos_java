eea109466ecb2ba715e042a32e29413b2cf10caf
neamedina73 <77454466+neamedina73@users.noreply.github.com>
2021-07-03 17:12:44 -0500

    Add files via upload

--- Ejercicios basicos de Java/OperacionesAritm.java
+++ Ejercicios basicos de Java/OperacionesAritm.java
@@ -8,17 +8,18 @@ public class OperacionesAritm{
         int num1 = 0;
         int num2 = 0;
         System.out.println("Ingrese el primer número: ");
-        num1 = numeros.nextInt();
+        num1 = numeros.nextInt();//captura los datos ingresados por teclado
         System.out.println("Ingrese el segundo número: ");
         num2 = numeros.nextInt();
         /*Realizamos las operaciones.
         * También podemos guardar los resultados en variables*/
-        System.out.println("=====================================");
+        System.out.println("              RESULTADOS:                 ");
+        System.out.println("=======================================");
         System.out.println("El resultado de la suma es: "+(num1+num2));
         System.out.println("El resultado de la resta es: "+(num1-num2));
         System.out.println("El resultado de la multilicación es: "+(num1*num2));
         System.out.println("El resultado de la división es: "+(num1/num2));
-        System.out.println("=====================================");
+        System.out.println("=======================================");
         numeros.close();
         }
 }
