40eed2c3a4cab7e907b6caad81b369c411ce2850
neamedina73 <77454466+neamedina73@users.noreply.github.com>
2021-07-03 19:42:37 -0500

    Add files via upload

--- Ejercicios basicos de Java/sueldo_Trabajador.java
+++ Ejercicios basicos de Java/sueldo_Trabajador.java
@@ -5,10 +5,7 @@ import java.util.Scanner;
 public class sueldo_Trabajador {
     public static void main(String[] args) {
         Scanner horasExt = new Scanner(System.in);
-        int salario_base = 982523;
-        int sueldo_Total = 30;
-        int valor_hrasExt;
-        int cant_hrasExt;
+        int salario_base = 982523, sueldo_Total, valor_hrasExt, cant_hrasExt;
         System.out.print("Ingrese la cantidad de horas extras del mes: ");
         cant_hrasExt = horasExt.nextInt();
         valor_hrasExt = (cant_hrasExt * 30000);
@@ -17,8 +14,9 @@ public class sueldo_Trabajador {
         System.out.println("           N  O  M  I  N  A                \n");
         System.out.println("==============================================");
         System.out.println("El salario base es: $ "+salario_base);
-        System.out.println("Las horas extras laboradas fueron: "+cant_hrasExt+" horas");
-        System.out.println("El valor de las horas extras es:  $ "+valor_hrasExt); 
+        System.out.println("Las horas extras laboradas fueron: "+cant_hrasExt+" horas.");
+        System.out.println("El valor de las horas extras es:  $ "+valor_hrasExt);
+        System.out.println("______________________________________________");
         System.out.println("Total a pagar al trabajador:  $ " +sueldo_Total);
         System.out.println("==============================================");
         horasExt.close();
