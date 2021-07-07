20714edcd1b1fc3dabfa34b12662f5b237ad76b3
neamedina73 <77454466+neamedina73@users.noreply.github.com>
2021-07-07 11:56:48 -0500

    Add files via upload

---
+++ miprimerProyecto/src/main/java/MiprimeraClase.java
@@ -0,0 +1,15 @@
+/*
+ * Proyecto elaborado por Hola Mundo
+ */
+
+/**
+ *
+ * @author Nelson Acuña Medina
+ */
+//Nuestra primera clase
+public class MiprimeraClase {
+    //mi primer metódo
+    public static void main (String args[]){
+        System.out.println("Hola Mundo");   
+}
+}
